097db374d00e1c66e3cca2aeae1f1b466b2f9178
leiweibo <leiweibo@gmail.com>
2019-09-08 20:03:16 +0800

    完善数据解析

--- fundspider/fundspider/items.py
+++ fundspider/fundspider/items.py
@@ -9,16 +9,23 @@ import scrapy
 
 
 class FundspiderItem(scrapy.Item):
-    # 顺序
-    order = scrapy.Field()
     # 基金代码
     symbol = scrapy.Field()
     # 基金简称
     name = scrapy.Field()
+
+    # 手续费
+    subscription_fee = scrapy.Field()
+
+class FundNetValueItem(scrapy.Item):
+    symbol = scrapy.Field()
+    # 净值日期
+    fund_date = scrapy.Field()
+    # 当日净值
+    fund_net_value = scrapy.Field()
+    # 累计净值
+    fund_accu_net_value = scrapy.Field()
     # 赎回状态
     redemption_status = scrapy.Field()
     # 申购状态
     subscription_status = scrapy.Field()
-    # 手续费
-    subscription_fee = scrapy.Field()
-    pass
--- fundspider/fundspider/spiders/fundspider.py
+++ fundspider/fundspider/spiders/fundspider.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 import scrapy
-from fundspider.items import FundspiderItem
+from fundspider.items import FundspiderItem, FundNetValueItem
 import json
 import re
 from datetime import datetime
@@ -12,7 +12,7 @@ class fundspider(scrapy.Spider):
     allowed_domains = ["www.eastmoney.com/", "fund.eastmoney.com"]
     start_urls = [
         # 'http://fund.eastmoney.com/js/fundcode_search.js'
-        f'http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=kf&ft=qdii&rs=&gs=0&sc=zzf&st=desc&pi=1&pn=100&dx=1'
+        f'http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=kf&ft=bb&rs=&gs=0&sc=zzf&st=desc&pi=1&pn=3&dx=1'
     ]
 
 
@@ -23,23 +23,32 @@ class fundspider(scrapy.Spider):
 
         allpages = re.findall('allPages:(\d*)', rawResponse)[0]
         currentPage = re.findall('pageIndex:(\d*)', rawResponse)[0]
-        print(f'all pages ---->{allpages}, current page->{currentPage}')
         dataJsonReponse = re.findall('var rankData = {datas:(.*])', rawResponse)[0]
         jsonResponse = json.loads(dataJsonReponse)
+        count = 0;
         for response in jsonResponse:
             array = response.split(',')
-            print(array[0] + array[1])
             dt = datetime.now()
-            targetFundNetValueUrl = f'http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code=110022&sdate=2004-01-01&edate={dt.strftime("%Y-%m-%d")}&per=20&page=1'
-            yield scrapy.Request(targetFundNetValueUrl, self.parseHistoryNetvalue)
+            targetFundNetValueUrl = f'http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code={array[0]}&sdate=2004-01-01&edate={dt.strftime("%Y-%m-%d")}&per=20&page=1'
+            item = FundspiderItem(symbol=array[0], name=array[1])
+            yield item
+            request = scrapy.FormRequest(targetFundNetValueUrl, self.parseHistoryNetvalue)
+            request.meta['fund_code'] = array[0]
+            count += 1
+            request.meta['count'] = (count)
+            yield request
 
         
-        # if (int(currentPage) < int(allpages)):
-        #     targetFundListUrl = f'http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=kf&ft=qdii&rs=&gs=0&sc=zzf&st=desc&pi={int(currentPage) + 1}&pn=100&dx=1'
-        #     print(f'最终的target: {targetFundListUrl}')
-        #     yield scrapy.Request(targetFundListUrl, self.parse)
+        if (int(currentPage) < int(allpages)):
+            targetFundListUrl = f'http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=kf&ft=qdii&rs=&gs=0&sc=zzf&st=desc&pi={int(currentPage) + 1}&pn=100&dx=1'
+            print(f'最终的target: {targetFundListUrl}')
+            yield scrapy.Request(targetFundListUrl, self.parse)
     
+    fund_code = ''
     def parseHistoryNetvalue(self, response):
+        global fund_code
+        if (response.meta.get('fund_code') != None):
+            fund_code = response.meta.get('fund_code')
         rawResponse = response.text
         soup = BeautifulSoup(rawResponse, 'html.parser')
         fundNetvalues = []
@@ -52,12 +61,14 @@ class fundspider(scrapy.Spider):
                     row_records.append('---')
                 else:
                     row_records.append(val[0])
-            print(f'{row_records[1]}    {row_records[2]}')
-            fundNetvalues.append(row_records)
-        pages = re.findall('pages:(\d*)', rawResponse)[0]
-        curpage = re.findall('curpage:(\d*)', rawResponse)[0]
-        print(f'{pages}  {curpage}')
+            item = FundNetValueItem(symbol = fund_code, fund_date = row_records[0], fund_net_value=row_records[1], fund_accu_net_value=row_records[2], redemption_status = row_records[5], subscription_status = row_records[4])
+            yield item
+            # print(f'{row_records[1]}    {row_records[2]}')
+
+            
+        pages = int(re.findall('pages:(\d*)', rawResponse)[0])
+        curpage = int(re.findall('curpage:(\d*)', rawResponse)[0])
         if (int(curpage) < int(pages)):
             dt = datetime.now()
-            targetFundNetValueUrl = f'http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code=110022&sdate=2004-01-01&edate={dt.strftime("%Y-%m-%d")}&per=20&page={int(curpage) + 1}'
+            targetFundNetValueUrl = f'http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code={fund_code}&sdate=2004-01-01&edate={dt.strftime("%Y-%m-%d")}&per=20&page={int(curpage) + 1}'
             yield scrapy.Request(targetFundNetValueUrl, self.parseHistoryNetvalue)
