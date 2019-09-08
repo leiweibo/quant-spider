429e1cb669f2090a33dbd3f2ccaf5602d6ec49ad
leiweibo <leiweibo@gmail.com>
2019-09-08 10:16:11 +0800

    从search.js获取所有的基金列表

--- fundspider/fundspider/spiders/fundspider.py
+++ fundspider/fundspider/spiders/fundspider.py
@@ -1,30 +1,26 @@
 # coding:utf-8
 import scrapy
 from fundspider.items import FundspiderItem
+import json
+import re
 
 class fundspider(scrapy.Spider):
     name = 'fund-spider'
     allowed_domains = ["http://www.eastmoney.com/", "http://fund.eastmoney.com"]
     start_urls = [
-        'http://fund.eastmoney.com/fund.html'
+        'http://fund.eastmoney.com/js/fundcode_search.js'
+        # 'http://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=kf&ft=gp&rs=&gs=0&sc=zzf&st=desc&pi=1&pn=200&dx=1'
     ]
+
+
 
     def parse(self, response):
         print(f'-------------------------start to parse reponse.....')
-        fundtrs = response.xpath('//*[@id="oTable"]/tbody/tr')
-        for tr in fundtrs:
-            order = tr.xpath('.//td[3]/text()').extract()[0]
-            symbol = tr.xpath('.//td[4]/text()').extract()[0]
-            name = tr.xpath('.//td[5]/nobr/a[1]/@title').extract()[0]
-            redemption_status = tr.xpath('.//td[12]/text()').extract()[0]
-            subscription_status = tr.xpath('.//td[13]/text()').extract()[0]
-            subscription_fee = tr.xpath('.//td[14]//a/text()').extract()
-            if len(subscription_fee) == 0:
-                final_sub_fee = '---'
-            else:
-                final_sub_fee = subscription_fee[0]
-            print(f'{order} {symbol} {name} {redemption_status} {subscription_status} {final_sub_fee}')
-            item = FundspiderItem(order=order, symbol = symbol, name=name, redemption_status = redemption_status, subscription_status = subscription_fee, subscription_fee = final_sub_fee)
-            yield item
+        rawResponse = response.text
 
+        rawJsonResposne = re.findall('var r = (.*])', rawResponse)[0]
+        jsonResponse = json.loads(rawJsonResposne)
+        
+        for response in jsonResponse:
+            print(response[0])
            
