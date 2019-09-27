fce4fae52aaf2755391c0e30fbf2d71db10455c5
leiweibo <leiweibo@gmail.com>
2019-09-27 23:44:30 +0800

    可以下载极验的两张图片

--- snowball/snowball_combination/snowball_combination/spiders/snowball_login.py
+++ snowball/snowball_combination/snowball_combination/spiders/snowball_login.py
@@ -1,6 +1,7 @@
 import time
 from io import BytesIO
 from PIL import Image
+import base64
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
@@ -24,60 +25,10 @@ class CrackGeetest():
         self.email = EMAIL
         self.password = PASSWORD
     
-    def __del__(self):
-        self.browser.close()
-    
-    def get_geetest_button(self):
-        """
-        获取初始验证按钮
-        :return:
-        """
-        button = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'geetest_radar_tip')))
-        return button
-    
-    def get_position(self):
-        """
-        获取验证码位置
-        :return: 验证码位置元组
-        """
-        img = self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'geetest_canvas_img')))
-        time.sleep(2)
-        location = img.location
-        size = img.size
-        top, bottom, left, right = location['y'], location['y'] + size['height'], location['x'], location['x'] + size[
-            'width']
-        return (top, bottom, left, right)
-    
-    def get_screenshot(self):
-        """
-        获取网页截图
-        :return: 截图对象
-        """
-        screenshot = self.browser.get_screenshot_as_png()
-        screenshot = Image.open(BytesIO(screenshot))
-        return screenshot
-    
-    def get_slider(self):
-        """
-        获取滑块
-        :return: 滑块对象
-        """
-        slider = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'geetest_slider_button')))
-        return slider
-    
-    def get_geetest_image(self, name='captcha.png'):
-        """
-        获取验证码图片
-        :return: 图片对象
-        """
-        top, bottom, left, right = self.get_position()
-        print('验证码位置', top, bottom, left, right)
-        screenshot = self.get_screenshot()
-        captcha = screenshot.crop((left, top, right, bottom))
-        captcha.save(name)
-        return captcha
-    
-    def open(self):
+    '''
+    浏览器打开网址，并且填入账号密码，然后点击登录按钮，出现图形验证码
+    '''
+    def prepare_for_login(self):
         """
         打开网页输入用户名密码
         :return: None
@@ -92,110 +43,30 @@ class CrackGeetest():
         password.send_keys('')
         real_login_btn = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'Loginmodal_modal__login__btn_uk7')))
         real_login_btn.click()
-        time.sleep(20)
-        self.browser.quit()
-    
-    def get_gap(self, image1, image2):
-        """
-        获取缺口偏移量
-        :param image1: 不带缺口图片
-        :param image2: 带缺口图片
-        :return:
-        """
-        left = 60
-        for i in range(left, image1.size[0]):
-            for j in range(image1.size[1]):
-                if not self.is_pixel_equal(image1, image2, i, j):
-                    left = i
-                    return left
-        return left
-    
-    def is_pixel_equal(self, image1, image2, x, y):
-        """
-        判断两个像素是否相同
-        :param image1: 图片1
-        :param image2: 图片2
-        :param x: 位置x
-        :param y: 位置y
-        :return: 像素是否相同
-        """
-        # 取两个图片的像素点
-        pixel1 = image1.load()[x, y]
-        pixel2 = image2.load()[x, y]
-        threshold = 60
-        if abs(pixel1[0] - pixel2[0]) < threshold and abs(pixel1[1] - pixel2[1]) < threshold and abs(
-                pixel1[2] - pixel2[2]) < threshold:
-            return True
-        else:
-            return False
-    
-    def get_track(self, distance):
-        """
-        根据偏移量获取移动轨迹
-        :param distance: 偏移量
-        :return: 移动轨迹
-        """
-        # 移动轨迹
-        track = []
-        # 当前位移
-        current = 0
-        # 减速阈值
-        mid = distance * 4 / 5
-        # 计算间隔
-        t = 0.2
-        # 初速度
-        v = 0
-        
-        while current < distance:
-            if current < mid:
-                # 加速度为正2
-                a = 2
-            else:
-                # 加速度为负3
-                a = -3
-            # 初速度v0
-            v0 = v
-            # 当前速度v = v0 + at
-            v = v0 + a * t
-            # 移动距离x = v0t + 1/2 * a * t^2
-            move = v0 * t + 1 / 2 * a * t * t
-            # 当前位移
-            current += move
-            # 加入轨迹
-            track.append(round(move))
-        return track
-    
-    def move_to_gap(self, slider, track):
-        """
-        拖动滑块到缺口处
-        :param slider: 滑块
-        :param track: 轨迹
-        :return:
-        """
-        ActionChains(self.browser).click_and_hold(slider).perform()
-        for x in track:
-            ActionChains(self.browser).move_by_offset(xoffset=x, yoffset=0).perform()
-        time.sleep(0.5)
-        ActionChains(self.browser).release().perform()
-    
-    def login(self):
-        """
-        登录
-        :return: None
-        """
-        submit = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'login-btn')))
-        submit.click()
-        time.sleep(10)
-        print('登录成功')
-    
+
+    '''
+    导出canvas到png图片中
+    '''
+    def save_captcha_img(self, img_name, class_name):
+        img = self.wait.until(EC.presence_of_element_located((By.CLASS_NAME, class_name)))
+        getImgJS = 'return document.getElementsByClassName("' + class_name + '")[0].toDataURL("image/png");'
+        img = self.browser.execute_script(getImgJS)
+        base64_data_img = img[img.find(',') + 1:]
+        image_base = base64.b64decode(base64_data_img)
+        file = open(img_name, 'wb')
+        file.write(image_base)
+        file.close()
+
     def crack(self):
-        # 输入用户名密码
-        self.open()
-        # # 点击验证按钮
-        # button = self.get_geetest_button()
-        # button.click()
-        # # 获取验证码图片
-        # image1 = self.get_geetest_image('captcha1.png')
+        # 访问网站并且输入用户名和密码，然后点击验证码按钮
+        self.prepare_for_login()
+        # 保存图片t
+        # image1 = self.get_geetest_image()
+        self.save_captcha_img('captcha1.png', 'geetest_canvas_fullbg')
+        self.save_captcha_img('captcha2.png','geetest_canvas_bg')
+
+        time.sleep(10)
+        self.browser.quit()
         # # 点按呼出缺口
         # slider = self.get_slider()
         # slider.click()
